690a493db4747113445d813dab525595b9f36ac3
Hugo Swift <codefinchy@gmail.com>
2020-10-02 15:30:24 +0800

    添加财务统计组件

---
+++ docs/.vuepress/components/financialStatistics.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>共 {{ total }} 元</p>
+</template>
+
+<script>
+export default {
+  name: "financialStatistics",
+  data() {
+    return {
+      total: 0,
+    };
+  },
+  mounted() {
+    this.total = Array.from(this.$parent.$el.querySelectorAll("tbody > tr"))
+      .map((x) => Number(x.children[2].innerText))
+      .reduce((acc, cnt) => cnt + acc);
+  },
+};
+</script>
